c90e1b88f7e140c3df9b593c43ebf732ba60d059
janvierjr <bernie@myheartsleeve.com>
2024-01-05 16:24:54 -0600

    create initial footer component

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+'use client';
+import styles from './footer.module.css';
+import Link from 'next/link';
+
+const Footer = () => {
+  return <footer className={styles.footer}>Footer</footer>;
+};
+
+export default Footer;
