3990f8a282e91a64a0bc25d0e7169e34737e2039
janvierjr <bernie@myheartsleeve.com>
2024-01-05 16:25:29 -0600

    create initial nav component

---
+++ src/app/components/Navigation/NavMenu.tsx
@@ -0,0 +1,11 @@
+"use client"
+import styles from './navmenu.module.css';
+import Link from 'next/link';
+
+const NavMenu = () => {
+  return (
+      <nav className={styles.navigation}>Navigation Bar</nav>
+  );
+}
+
+export default NavMenu;
