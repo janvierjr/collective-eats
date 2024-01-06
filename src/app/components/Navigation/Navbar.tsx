223c77f36ec15e0aabc3a01ffc75cf9bc974271b
janvierjr <bernie@myheartsleeve.com>
2024-01-05 23:33:26 -0600

    update nav menu and hamburger menu active state

--- src/app/components/Navigation/Navbar.tsx
+++ src/app/components/Navigation/Navbar.tsx
@@ -13,38 +13,50 @@ const Navbar = () => {
   const [activeNav, setActiveNav] = useState(false);
   const [activeIndex, setActiveIndex] = useState(-1);
 
+  const handleMenuOpen = () => {
+    setActiveNav(!activeNav);
+
+  }
+
   return (
     <header>
       <nav className={styles.navigation}>
         <div className={'max-container'}>
-          <div className='nav-menu-wrapper'>
+          <div className='nav__menu_wrapper'>
             <button
               className={styles.hamburgerButton}
               aria-haspopup='true'
-              aria-expanded={activeNav}
               type='button'
-              onClick={() => setActiveNav(!activeNav)}
+              onClick={handleMenuOpen}
             >
-              <div className={styles.topLine}></div>
-              <div className={styles.bottomLine}></div>
+              <div
+                className={`topLine ${activeNav ? 'active' : ''}`}
+              ></div>
+              <div
+                className={`bottomLine ${activeNav ? 'active' : ''}`}
+              ></div>
             </button>
-            <div
-              className={`${activeNav ? 'active' : ''} nav-menu`}
-              role='menu'
-            >
-              {NAV_MENU_LIST.map((menu, index) => {
-                return (
-                  <div
-                    onClick={() => {
-                      setActiveIndex(index)
-                      setActiveNav(false)
-                    }}
-                    key={menu.text}>
-                    <NavItem active={activeIndex === index} {...menu} />
-                  </div>
-                );
-              })}
-            </div>
+            {activeNav && (
+              <div
+                className={`nav__menu ${activeNav ? 'active' : ''}`}
+                role='menu'
+                aria-expanded={activeNav}
+              >
+                {NAV_MENU_LIST.map((menu, index) => {
+                  return (
+                    <div
+                      onClick={() => {
+                        setActiveIndex(index);
+                        setActiveNav(false);
+                      }}
+                      key={menu.text}
+                    >
+                      <NavItem active={activeIndex === index} {...menu} />
+                    </div>
+                  );
+                })}
+              </div>
+            )}
           </div>
         </div>
         <div className={styles.searchContainer}>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './components/Navigation/Navbar';
 import Footer from './components/Footer/Footer';
+import localFont from 'next/font/local';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
+const brice =  localFont({src: '../../public/fonts/Brice-BlackSemiExpanded.otf'})
 
 export const metadata: Metadata = {
   title: 'Collective Eats',
